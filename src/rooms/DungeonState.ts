dc7fb70809f5e0a0bd34b43a438bbec9ed622315
ปราชญ์ หาญ <prachhankiet@gmail.com>
2023-08-05 14:23:56 +0700

    Fix

--- src/rooms/DungeonState.ts
+++ src/rooms/DungeonState.ts
@@ -76,6 +76,6 @@ export class DungeonState extends Schema {
   @type("boolean") characterBuff1: boolean = false;
   @type("boolean") characterBuff2: boolean = false;
 
-  @type([ArraySchema.with(Boolean)]) buffs: ArraySchema<ArraySchema<boolean>> = new ArraySchema<ArraySchema<boolean>>();
+  @type([[Boolean]]) buffs: ArraySchema<ArraySchema<boolean>> = new ArraySchema<ArraySchema<boolean>>();
 }
 
