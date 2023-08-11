ab36ca7bf527a90d15265174634d88b2e0b3f753
ปราชญ์ หาญ <prachhankiet@gmail.com>
2023-08-11 18:05:07 +0700

    Update TreasureRoom.ts

--- src/rooms/TreasureRoom.ts
+++ src/rooms/TreasureRoom.ts
@@ -134,7 +134,7 @@ export class TreasureRoom extends Room<DungeonState> {
 // Check and apply 2-characters buff combinations for all teams
 for (let teamIndex = 1; teamIndex <= 3; teamIndex++) {
 
-  this.state.buffDescriptions[teamIndex] = [];
+  this.state.buffDescriptions.push("");
 
 
   for (const combinationName in buffCombinations) {
@@ -166,23 +166,23 @@ for (let teamIndex = 1; teamIndex <= 3; teamIndex++) {
           switch (combinationName) {
             case "Swordman+Swordman":
               character.def += 20;
-              this.state.buffDescriptions[teamIndex].push("Def +20\n")
+              this.state.buffDescriptions[teamIndex] += ("Def +20\n")
               break;
             case "Lancer+Lancer":
               character.atk += 35;
-              this.state.buffDescriptions[teamIndex].push("Atk +35\n")
+              this.state.buffDescriptions[teamIndex] += ("Atk +35\n")
               break;
             case "Archer+Archer":
               character.hit += 10;
-              this.state.buffDescriptions[teamIndex].push("Hit +10\n")
+              this.state.buffDescriptions[teamIndex] += ("Hit +10\n")
               break;
             case "Magician+Magician":
               character.mAtk += 30;
-              this.state.buffDescriptions[teamIndex].push("mAtk +30\n")
+              this.state.buffDescriptions[teamIndex] += ("mAtk +30\n")
               break;
             case "Acolyte+Acolyte":
               character.hpMAX += 80;
-              this.state.buffDescriptions[teamIndex].push("Max HP +80\n")
+              this.state.buffDescriptions[teamIndex] += ("Max HP +80\n")
               break;
             // Add more cases for other 2-characters buffs here
             // ...
